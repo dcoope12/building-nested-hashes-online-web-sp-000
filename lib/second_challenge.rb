2c6f9d15a4b8080ee187e4a1bd00593a957cbc33
Devin Cooper <dcoope12@asu.edu>
2020-09-17 00:29:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,25 +3,25 @@ def second_challenge
     :montague => {
       :patriarch{
 
-      }
+      },
       :matriarch{
 
-      }
+      },
       :hero{
 
-      }
+      },
       :hero_friends[]
       },
     :capulet => {
       :patriarch{
 
-      }
+      },
       :matriarch{
 
-      }
+      },
       :heroine{
 
-      }
+      },
       :heroine_friends[]
       }}
 
