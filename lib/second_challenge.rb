6441a4e13c7d4e36def1dc723e39eb52d19ac0e5
Devin Cooper <dcoope12@asu.edu>
2020-09-17 00:18:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -13,7 +13,16 @@ def second_challenge
       :hero_friends[]
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch{
+        
+      }
+      :matriarch{
+        
+      }
+      :heroine{
+        
+      }
+      :heroine_friends[]
       }}
 
 end
